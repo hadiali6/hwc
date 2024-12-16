670f81364d21a89de612b4a687552e0065318862
Hadi Ali <hadidub2006@gmail.com>
2024-12-16 00:33:17 -0800

    add stylua.toml and update definitions+example config

--- script/definitions.lua
+++ script/definitions.lua
@@ -2,39 +2,39 @@
 
 hwc = {}
 
----Terminate the compositor.
+---Terminate the compositor
 function hwc.exit() end
 
----Reload configuration file.
+---Reload configuration file
 function hwc.reload() end
 
----@param cmd string # command to run.
----@param request_pid boolean? # if true, get pid of created process.
----@param request_stdin_fd boolean? # if true, get file descriptor of the process stdin.
----@param request_stdout_fd boolean? # if true, get file descriptor of the process stdout.
----@param request_stderr_fd boolean? # if true, get file descriptor of the process stderr.
----@return integer? # pid if requested.
----@return integer? # stdin_fd if requested.
----@return integer? # stdout_fd if requested.
----@return integer? # stderr_fd if requested.
+---@param cmd string # command to run
+---@param request_pid boolean? # if true, get pid of created process
+---@param request_stdin_fd boolean? # if true, get file descriptor of the process stdin
+---@param request_stdout_fd boolean? # if true, get file descriptor of the process stdou.
+---@param request_stderr_fd boolean? # if true, get file descriptor of the process stderr
+---@return integer? # pid if requested
+---@return integer? # stdin_fd if requested
+---@return integer? # stdout_fd if requested
+---@return integer? # stderr_fd if requested
 function hwc.spawn(cmd, request_pid, request_stdin_fd, request_stdout_fd, request_stderr_fd) end
 
----@param key string # key.
----@param modifiers string # modifiers.
----@param is_repeat boolean # repeated if held.
----@param is_on_release boolean # executed on release instead of press.
----@param layout_index integer? # xkb layout index.
----@param callback fun() # callback called when keybind is pressed.
----@return integer # integer ID representing index inside keybind list.
+---@param key string # key
+---@param modifiers string # modifiers
+---@param is_repeat boolean # repeated if held
+---@param is_on_release boolean # executed on release instead of press
+---@param layout_index integer? # xkb layout index
+---@param callback fun() # callback called when keybind is pressed
+---@return integer # integer ID representing index inside keybind list
 function hwc.add_keybind(key, modifiers, is_repeat, is_on_release, layout_index, callback) end
 
----@param id integer # integer ID representing index inside keybind list.
----@return boolean # true if removed, false if not removed.
+---@param id integer # integer ID representing index inside keybind list
+---@return boolean # true if removed, false if not removed
 function hwc.remove_keybind_by_id(id) end
 
----@param key string # key.
----@param modifiers string # modifiers.
----@return boolean # true if removed, false if not removed.
+---@param key string # key
+---@param modifiers string # modifiers
+---@return boolean # true if removed, false if not removed
 function hwc.remove_keybind(key, modifiers) end
 
 ---@alias hwc.Output.Backend
@@ -62,14 +62,27 @@ function hwc.remove_keybind(key, modifiers) end
 ---@field height integer #
 ---@field refresh_rate integer #
 ---@field enabled boolean #
----@field adaptive_scale_enabled boolean #
+---@field adaptive_sync_supported boolean #
+---@field adaptive_sync_status boolean #
 ---@field scale boolean #
 ---@field transform hwc.Output.Transform #
 
----**Create a new output.**
----> Only available in Wayland, Headless, and X11 backends.
----> Generally intended for developer use for testing multi-monitor functionality.
----> Headless outputs can also be useful when dealing with VNC applications.
+---**Create a new output**
+---> Only available in Wayland, Headless, and X11 backends
+---> This function will raise an error when using the DRM backend (aka normal use through tty or display manager)
+---> Generally intended for developer use for testing multi-monitor functionality
+---> Headless outputs can also be useful when dealing with VNC applications
+---> See [wlroots documentation about backends](https://gitlab.freedesktop.org/wlroots/wlroots/-/blob/master/docs/architecture.md?ref_type=heads#backends)
+---
+---Example Usage:
+---```lua
+----- use pcall as this function can raise an error
+---local ok, output = pcall(hwc.create_output)
+---if ok then
+---    -- do something with returned hwc.Output
+---    print("output: " .. output.name .. " created!")
+---end
+---```
 ---
 ---Under the hood, this uses the following wlroots functions for their corresponding backends:
 ---```c
